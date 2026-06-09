42d6a08d06dfcc89ffc1e74c2ca9eb020a3ab6fb
timbainbridge <tfbainbridge@gmail.com>
2026-06-09 16:05:48 +1000

    Partially added bifactor models to esem.from.models.

--- R/esem.from.mods.R
+++ R/esem.from.mods.R
@@ -1,19 +1,21 @@
-#' Runs ESEM models based on CFA and EFA model outputs and keys.
+#' Runs Exploratory Structural Equation Models based on Confirmatory and
+#' Exploratory Factor Analyses model outputs and keys.
 #'
-#' esem.from.keys runs a exploratory structural equation model (ESEM) in lavaan
-#' where the explorator factor analysis (EFA) factors predict CFA factors in
-#' separate models for each CFA factor.
+#' esem.from.keys runs an exploratory structural equation model (ESEM) in lavaan
+#' where the exploratory factor analysis (EFA) factors predict confirmatory
+#' factor analysis factors in separate models for each CFA factor.
 #' The function uses outputs from `cfa.from.keys()` and `efa.from.keys()` and
-#' the same keys as those used in the functions.
+#' the same keys as those used in those upstream functions, so cannot be run
+#' without first using those upstream functions.
 #'
-#' @param cfa_name
-#' Must match `name` used in a previously run `cfa.from.keys` function call.
 #' @param efa_name
 #' Must match `name` used in a previously run `efa.from.keys` function call.
-#' @param cfa_keys
-#' Must match `keys` used in a previously run `cfa.from.keys` function call.
+#' @param cfa_name
+#' Must match `name` used in a previously run `cfa.from.keys` function call.
 #' @param efa_keys
 #' Must match `keys` used in a previously run `efa.from.keys` function call.
+#' @param cfa_keys
+#' Must match `keys` used in a previously run `cfa.from.keys` function call.
 #' @param d
 #' The data. This must include all observed variables used in any of the models.
 #' @param name A name for the collection of models. Defaults to 'esem'.
@@ -58,18 +60,22 @@
 # TODO: Add bifactor model functionality.
 
 esem.from.mods <- function(
-    cfa_name, efa_name, cfa_keys, efa_keys, d, name = "esem",
-    out_dir = "output", fit_save = FALSE, fit_measures = NULL, miss = "ML",
-    hash_dir = "hashes", mod_dir = NULL
+    efa_name, cfa_name = NULL, bif_name = NULL, efa_keys, cfa_keys = NULL,
+    bif_keys_g = NULL, bif_keys_b = NULL, bif_keys = NULL,
+    d, name = "esem", out_dir = "output", fit_save = FALSE, fit_measures = NULL,
+    miss = "ML", hash_dir = "hashes", mod_dir = NULL
 ) {
-  # TODO: Add checks for cfa_name or cfa_fit and that inputs are correct.
   if (
     !file.exists(file.path(out_dir, cfa_name, paste0(cfa_name, "_par.rds")))
   ) {
     stop(
       paste0(
         cfa_name, "_par.rds", " not found at ",
-        file.path(out_dir, cfa_name, paste0(cfa_name, "_par.rds"))
+        file.path(out_dir, cfa_name, paste0(cfa_name, "_par.rds")),
+        ". You will need to run at least one CFA model using",
+        "`cfa.from.keys()` for this object to be saved.",
+        "If you have done that, ensure that `cfa_name` here matches the one",
+        "you used to run the CFA models originally."
       )
     )
   }
@@ -81,17 +87,72 @@ esem.from.mods <- function(
     stop(
       paste0(
         efa_name, "_par.rds", " not found at ",
-        file.path(out_dir, efa_name, paste0(efa_name, "_par.rds"))
+        file.path(out_dir, efa_name, paste0(efa_name, "_par.rds")),
+        ". You will need to run an EFA model using",
+        "`efa.from.keys()` for this object to be saved.",
+        "If you have done that, ensure that `efa_name` here matches the one",
+        "you used to run the efa model originally."
       )
     )
   }
   efa_par <-
     readRDS(file.path(out_dir, efa_name, paste0(efa_name, "_par.rds")))
+  if (
+    !file.exists(file.path(out_dir, bif_name, paste0(bif_name, "_par.rds")))
+  ) {
+    stop(
+      paste0(
+        bif_name, "_par.rds", " not found at ",
+        file.path(out_dir, bif_name, paste0(bif_name, "_par.rds")),
+        ". You will need to run at least one bifactor model using",
+        "`bifactor.from.keys()` for this object to be saved.",
+        "If you have done that, ensure that `bif_name` here matches the one",
+        "you used to run the bifactor models originally."
+      )
+    )
+  }
+  bif_par <-
+    readRDS(file.path(out_dir, bif_name, paste0(bif_name, "_par.rds")))
   # TODO: Check that the _par.rds files matches expectations.
-  mods <- mapply(
+  if (is.null(cfa_name) & is.null(bif_name)) {
+    stop("Either ")
+  }
+  if (!is.null(cfa_name)) {
+    mods_cfa <- mapply(
+      function(x, sn) {
+        if (length(x) > 1) {
+          x0 <- cfa_par[[sn]]
+          tmp0 <- x0[x0$op %in% c("=~", "~~"), c("lhs", "op", "rhs", "est")]
+          tmp1 <- tmp0[!(sn == tmp0$lhs & sn == tmp0$rhs), ]
+          tmp <- paste0(
+            paste(tmp1$lhs, tmp1$op, tmp1$est, "*", tmp1$rhs, collapse = "\n")
+          )
+        } else {
+          tmp <- NULL
+        }
+        tmpe0 <- efa_par[[efa_name]]
+        tmpe <- tmpe0[tmpe0$op %in% c("=~", "~~"), c("lhs", "op", "rhs", "est")]
+        paste0(
+          c(
+            # EFA as CFA
+            paste(tmpe$lhs, tmpe$op, tmpe$est, "*", tmpe$rhs, collapse = "\n"),
+            # Scale CFA or Latent variable correlations (if only 1 item)
+            if (length(x) != 1) tmp else {
+              paste0(sn, " ~~ ", paste0(names(efa_keys), collapse = " + "))
+            },
+            # Regression
+            paste(sn, "~", paste0(names(efa_keys), collapse = " + "))
+          ),
+          collapse = "\n"
+        )
+      },
+      x = cfa_keys, sn = names(cfa_keys), SIMPLIFY = FALSE
+    )
+  }
+  mods_bif <- mapply(
     function(x, sn) {
       if (length(x) > 1) {
-        x0 <- cfa_par[[sn]]
+        x0 <- bif_par[[sn]]
         tmp0 <- x0[x0$op %in% c("=~", "~~"), c("lhs", "op", "rhs", "est")]
         tmp1 <- tmp0[!(sn == tmp0$lhs & sn == tmp0$rhs), ]
         tmp <- paste0(
@@ -116,8 +177,9 @@ esem.from.mods <- function(
         collapse = "\n"
       )
     },
-    x = cfa_keys, sn = names(cfa_keys), SIMPLIFY = FALSE
+    x = bif_keys, sn = names(bif_keys), SIMPLIFY = FALSE
   )
+  mods <- c(mods_cfa, mods_bif)
   mod_out <- sem.check(
     mods,
     d,
