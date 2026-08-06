6782c911ca02ef1d2f97f277fcbe1e4daa531a0f
timbainbridge <tfbainbridge@gmail.com>
2026-08-06 18:27:20 +1000

    Changed efa example to be quicker.

--- R/efa.from.keys.R
+++ R/efa.from.keys.R
@@ -74,11 +74,14 @@
 #'   },
 #'   simplify = FALSE
 #' )
-#' # Run model
-#' efa_fit <- efa.from.keys(keys_e, BFIGritHope, check = FALSE, fit_save = TRUE)
+#' # Run model with selected fit measures.
+#' efa_fit <- efa.from.keys(
+#'   keys_e, BFIGritHope, check = FALSE,
+#'   fit_save = TRUE, fit_measures = c("cfi", "rmsea")
+#' )
 #' # Examine results
-#' summary(efa_fit$fit$efa)                   # Standard lavaan summary
-#' efa_fit$fit_measures[, c("cfi", "rmsea")]  # Fit measures
+#' summary(efa_fit$fit$efa)  # Standard lavaan summary
+#' efa_fit$fit_measures      # Fit measures
 
 efa.from.keys <- function(
     keys, data, orthogonal = FALSE, fit_save = TRUE, fit_measures = "all",
